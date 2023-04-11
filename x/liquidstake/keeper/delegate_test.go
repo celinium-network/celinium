b282c50adce957d5bebffb7f136c332d05d6e0cb
byterui <shirui_yj133@163.com>
2023-04-11 12:03:59 +0800

    improve

--- x/liquidstake/keeper/delegate_test.go
+++ x/liquidstake/keeper/delegate_test.go
@@ -6,13 +6,8 @@ import (
 	epochtypes "github.com/celinium-netwok/celinium/x/epochs/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
-	ibccommitmenttypes "github.com/cosmos/ibc-go/v6/modules/core/23-commitment/types"
-	ibchost "github.com/cosmos/ibc-go/v6/modules/core/24-host"
-	ibctesting "github.com/cosmos/ibc-go/v6/testing"
-	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/celinium-netwok/celinium/x/liquidstake/types"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 func (suite *KeeperTestSuite) TestCreateNewDelegationRecordAtEpochStart() {
@@ -88,7 +83,7 @@ func (suite *KeeperTestSuite) TestProcessDelegation() {
 	suite.sourceChain.Coordinator.CurrentTime = coordTime
 
 	nextBlockTime := coordTime
-	_, nextBlockBeginRes := NextBlockWithRes(suite.controlChain, nextBlockTime)
+	_, nextBlockBeginRes := nextBlockWithRes(suite.controlChain, nextBlockTime)
 
 	suite.controlChain.NextBlock()
 	suite.transferPath.EndpointA.UpdateClient()
@@ -127,79 +122,6 @@ func (suite *KeeperTestSuite) TestProcessDelegation() {
 	suite.Equal(sc.StakedAmount, testCoin.Amount)
 }
 
-func chainRecvPacket(chain *ibctesting.TestChain, endpoint *ibctesting.Endpoint, msgRecvpacket *channeltypes.MsgRecvPacket) (*channeltypes.MsgAcknowledgement, error) {
-	ctx := chain.GetContext()
-	getCeliniumApp(chain)
-	if _, err := getCeliniumApp(chain).IBCKeeper.RecvPacket(ctx, msgRecvpacket); err != nil {
-		return nil, err
-	}
-
-	chain.NextBlock()
-	endpoint.UpdateClient()
-
-	return assembleAckPacketFromEvents(chain, msgRecvpacket.Packet, ctx.EventManager().Events())
-}
-
-func chainRecvAck(chain *ibctesting.TestChain, endpoint *ibctesting.Endpoint, ack *channeltypes.MsgAcknowledgement) (*channeltypes.MsgRecvPacket, error) {
-	ctx := chain.GetContext()
-	getCeliniumApp(chain)
-	if _, err := getCeliniumApp(chain).IBCKeeper.Acknowledgement(ctx, ack); err != nil {
-		return nil, err
-	}
-
-	chain.NextBlock()
-	endpoint.UpdateClient()
-
-	return assembleRecvPacketByEvents(chain, ctx.EventManager().Events())
-}
-
-func assembleRecvPacketByEvents(chain *ibctesting.TestChain, events sdk.Events) (*channeltypes.MsgRecvPacket, error) {
-	packet, err := ibctesting.ParsePacketFromEvents(events)
-	if err != nil {
-		return nil, err
-	}
-
-	commitKey := ibchost.PacketCommitmentKey(packet.SourcePort, packet.SourceChannel, packet.Sequence)
-	proof, height := chain.QueryProof(commitKey)
-
-	backProofType := ibccommitmenttypes.MerkleProof{}
-	backProofType.Unmarshal(proof)
-
-	msgRecvPacket := channeltypes.MsgRecvPacket{
-		Packet:          packet,
-		ProofCommitment: proof,
-		ProofHeight:     height,
-		Signer:          chain.SenderAccount.GetAddress().String(),
-	}
-
-	return &msgRecvPacket, nil
-}
-
-func assembleAckPacketFromEvents(chain *ibctesting.TestChain, packet channeltypes.Packet, events sdk.Events) (*channeltypes.MsgAcknowledgement, error) {
-	ack, err := ibctesting.ParseAckFromEvents(events)
-	if err != nil {
-		return nil, err
-	}
-	key := ibchost.PacketAcknowledgementKey(packet.GetDestPort(),
-		packet.GetDestChannel(),
-		packet.GetSequence())
-
-	proof, height := chain.QueryProof(key)
-
-	backProofType := ibccommitmenttypes.MerkleProof{}
-	backProofType.Unmarshal(proof)
-
-	ackMsg := channeltypes.MsgAcknowledgement{
-		Packet:          packet,
-		Acknowledgement: ack,
-		ProofAcked:      proof,
-		ProofHeight:     height,
-		Signer:          chain.SenderAccount.GetAddress().String(),
-	}
-
-	return &ackMsg, nil
-}
-
 func (suite *KeeperTestSuite) startDelegationEpoch(sourceChain *types.SourceChain) *epochtypes.EpochInfo {
 	controlChainApp := getCeliniumApp(suite.controlChain)
 
@@ -233,71 +155,4 @@ func (suite *KeeperTestSuite) startDelegationEpoch(sourceChain *types.SourceChai
 	suite.transferPath.EndpointA.UpdateClient()
 
 	return &epochInfo
-}
-
-func parseMsgRecvPacketFromEvents(fromChain *ibctesting.TestChain, events []abci.Event, sender string) []channeltypes.MsgRecvPacket {
-	var msgRecvPackets []channeltypes.MsgRecvPacket
-	for _, ev := range events {
-		events := sdk.Events{sdk.Event{
-			Type:       ev.Type,
-			Attributes: ev.Attributes,
-		}}
-
-		packet, err := ibctesting.ParsePacketFromEvents(events)
-		if err != nil {
-			continue
-		}
-
-		commitKey := ibchost.PacketCommitmentKey(packet.SourcePort, packet.SourceChannel, packet.Sequence)
-		proof, height := fromChain.QueryProof(commitKey)
-
-		backProofType := ibccommitmenttypes.MerkleProof{}
-		backProofType.Unmarshal(proof)
-
-		msgRecvPacket := channeltypes.MsgRecvPacket{
-			Packet:          packet,
-			ProofCommitment: proof,
-			ProofHeight:     height,
-			Signer:          sender,
-		}
-
-		msgRecvPackets = append(msgRecvPackets, msgRecvPacket)
-	}
-
-	return msgRecvPackets
-}
-
-// next block with res
-func NextBlockWithRes(chain *ibctesting.TestChain, nextBlockTime time.Time) (abci.ResponseEndBlock, abci.ResponseBeginBlock) {
-	endBlockres := chain.App.EndBlock(abci.RequestEndBlock{Height: chain.CurrentHeader.Height})
-
-	chain.App.Commit()
-
-	// set the last header to the current header
-	// use nil trusted fields
-	chain.LastHeader = chain.CurrentTMClientHeader()
-
-	// val set changes returned from previous block get applied to the next validators
-	// of this block. See tendermint spec for details.
-	chain.Vals = chain.NextVals
-	chain.NextVals = ibctesting.ApplyValSetChanges(chain.T, chain.Vals, endBlockres.ValidatorUpdates)
-
-	// increment the current header
-	chain.CurrentHeader = tmproto.Header{
-		ChainID: chain.ChainID,
-		Height:  chain.App.LastBlockHeight() + 1,
-		AppHash: chain.App.LastCommitID().Hash,
-		// NOTE: the time is increased by the coordinator to maintain time synchrony amongst
-		// chains.
-		Time:               chain.CurrentHeader.Time,
-		ValidatorsHash:     chain.Vals.Hash(),
-		NextValidatorsHash: chain.NextVals.Hash(),
-		ProposerAddress:    chain.CurrentHeader.ProposerAddress,
-	}
-
-	chain.CurrentHeader.Time = nextBlockTime
-	beginBlockRes := chain.App.BeginBlock(abci.RequestBeginBlock{Header: chain.CurrentHeader})
-
-	chain.NextBlock()
-	return endBlockres, beginBlockRes
 }
--- x/liquidstake/keeper/utils_test.go
+++ x/liquidstake/keeper/utils_test.go
@@ -3,13 +3,18 @@ package keeper_test
 import (
 	"time"
 
-	"github.com/celinium-netwok/celinium/app/params"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	ibcclienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibccommitmenttypes "github.com/cosmos/ibc-go/v6/modules/core/23-commitment/types"
 	ibchost "github.com/cosmos/ibc-go/v6/modules/core/24-host"
 	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+
+	"github.com/celinium-netwok/celinium/app/params"
 )
 
 func (suite *KeeperTestSuite) TestIBCTransfer() {
@@ -110,4 +115,144 @@ func mintCoin(
 		ibctransfertypes.ModuleName,
 		to,
 		sdk.NewCoins(coin))
+}
+
+func chainRecvPacket(chain *ibctesting.TestChain, endpoint *ibctesting.Endpoint, msgRecvpacket *channeltypes.MsgRecvPacket) (*channeltypes.MsgAcknowledgement, error) {
+	ctx := chain.GetContext()
+	getCeliniumApp(chain)
+	if _, err := getCeliniumApp(chain).IBCKeeper.RecvPacket(ctx, msgRecvpacket); err != nil {
+		return nil, err
+	}
+
+	chain.NextBlock()
+	endpoint.UpdateClient()
+
+	return assembleAckPacketFromEvents(chain, msgRecvpacket.Packet, ctx.EventManager().Events())
+}
+
+func chainRecvAck(chain *ibctesting.TestChain, endpoint *ibctesting.Endpoint, ack *channeltypes.MsgAcknowledgement) (*channeltypes.MsgRecvPacket, error) {
+	ctx := chain.GetContext()
+	getCeliniumApp(chain)
+	if _, err := getCeliniumApp(chain).IBCKeeper.Acknowledgement(ctx, ack); err != nil {
+		return nil, err
+	}
+
+	chain.NextBlock()
+	endpoint.UpdateClient()
+
+	return assembleRecvPacketByEvents(chain, ctx.EventManager().Events())
+}
+
+func assembleRecvPacketByEvents(chain *ibctesting.TestChain, events sdk.Events) (*channeltypes.MsgRecvPacket, error) {
+	packet, err := ibctesting.ParsePacketFromEvents(events)
+	if err != nil {
+		return nil, err
+	}
+
+	commitKey := ibchost.PacketCommitmentKey(packet.SourcePort, packet.SourceChannel, packet.Sequence)
+	proof, height := chain.QueryProof(commitKey)
+
+	backProofType := ibccommitmenttypes.MerkleProof{}
+	backProofType.Unmarshal(proof)
+
+	msgRecvPacket := channeltypes.MsgRecvPacket{
+		Packet:          packet,
+		ProofCommitment: proof,
+		ProofHeight:     height,
+		Signer:          chain.SenderAccount.GetAddress().String(),
+	}
+
+	return &msgRecvPacket, nil
+}
+
+func assembleAckPacketFromEvents(chain *ibctesting.TestChain, packet channeltypes.Packet, events sdk.Events) (*channeltypes.MsgAcknowledgement, error) {
+	ack, err := ibctesting.ParseAckFromEvents(events)
+	if err != nil {
+		return nil, err
+	}
+	key := ibchost.PacketAcknowledgementKey(packet.GetDestPort(),
+		packet.GetDestChannel(),
+		packet.GetSequence())
+
+	proof, height := chain.QueryProof(key)
+
+	backProofType := ibccommitmenttypes.MerkleProof{}
+	backProofType.Unmarshal(proof)
+
+	ackMsg := channeltypes.MsgAcknowledgement{
+		Packet:          packet,
+		Acknowledgement: ack,
+		ProofAcked:      proof,
+		ProofHeight:     height,
+		Signer:          chain.SenderAccount.GetAddress().String(),
+	}
+
+	return &ackMsg, nil
+}
+
+func parseMsgRecvPacketFromEvents(fromChain *ibctesting.TestChain, events []abci.Event, sender string) []channeltypes.MsgRecvPacket {
+	var msgRecvPackets []channeltypes.MsgRecvPacket
+	for _, ev := range events {
+		events := sdk.Events{sdk.Event{
+			Type:       ev.Type,
+			Attributes: ev.Attributes,
+		}}
+
+		packet, err := ibctesting.ParsePacketFromEvents(events)
+		if err != nil {
+			continue
+		}
+
+		commitKey := ibchost.PacketCommitmentKey(packet.SourcePort, packet.SourceChannel, packet.Sequence)
+		proof, height := fromChain.QueryProof(commitKey)
+
+		backProofType := ibccommitmenttypes.MerkleProof{}
+		backProofType.Unmarshal(proof)
+
+		msgRecvPacket := channeltypes.MsgRecvPacket{
+			Packet:          packet,
+			ProofCommitment: proof,
+			ProofHeight:     height,
+			Signer:          sender,
+		}
+
+		msgRecvPackets = append(msgRecvPackets, msgRecvPacket)
+	}
+
+	return msgRecvPackets
+}
+
+// next block with res
+func nextBlockWithRes(chain *ibctesting.TestChain, nextBlockTime time.Time) (abci.ResponseEndBlock, abci.ResponseBeginBlock) {
+	endBlockres := chain.App.EndBlock(abci.RequestEndBlock{Height: chain.CurrentHeader.Height})
+
+	chain.App.Commit()
+
+	// set the last header to the current header
+	// use nil trusted fields
+	chain.LastHeader = chain.CurrentTMClientHeader()
+
+	// val set changes returned from previous block get applied to the next validators
+	// of this block. See tendermint spec for details.
+	chain.Vals = chain.NextVals
+	chain.NextVals = ibctesting.ApplyValSetChanges(chain.T, chain.Vals, endBlockres.ValidatorUpdates)
+
+	// increment the current header
+	chain.CurrentHeader = tmproto.Header{
+		ChainID: chain.ChainID,
+		Height:  chain.App.LastBlockHeight() + 1,
+		AppHash: chain.App.LastCommitID().Hash,
+		// NOTE: the time is increased by the coordinator to maintain time synchrony amongst
+		// chains.
+		Time:               chain.CurrentHeader.Time,
+		ValidatorsHash:     chain.Vals.Hash(),
+		NextValidatorsHash: chain.NextVals.Hash(),
+		ProposerAddress:    chain.CurrentHeader.ProposerAddress,
+	}
+
+	chain.CurrentHeader.Time = nextBlockTime
+	beginBlockRes := chain.App.BeginBlock(abci.RequestBeginBlock{Header: chain.CurrentHeader})
+
+	chain.NextBlock()
+	return endBlockres, beginBlockRes
 }
