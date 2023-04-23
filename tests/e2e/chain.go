2a67a155aea118fcd9bb2c70a269080f20c3920d
byterui <shirui_yj133@163.com>
2023-04-23 15:29:35 +0800

    e2e test setup

--- tests/e2e/chain.go
+++ tests/e2e/chain.go
@@ -1,1 +1,174 @@
-package tests
+package e2e
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+
+	celiniumapp "github.com/celinium-network/celinium/app"
+)
+
+const (
+	keyringPassphrase = "testpassphrase"
+	keyringAppName    = "testnet"
+)
+
+type encodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Codec             codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
+
+type chain struct {
+	dataDir             string
+	ID                  string
+	validators          []*validator
+	accounts            []*account //nolint:unused
+	genesisAccounts     []*account
+	encfg               encodingConfig
+	InitBalance         string
+	Denom               string
+	ChainNodeBinary     string
+	ModuleBasicsGenesis func() (json.RawMessage, error)
+	DokcerImage         string
+}
+
+func newGaiaChain() (*chain, error) {
+	tmpDir, err := os.MkdirTemp("", "gaia-e2e-testnet-")
+	if err != nil {
+		return nil, err
+	}
+
+	// As we'll be connecting to real Gaia nodes, it's recommended to use the EncodingConfig from the Gaia app.
+	// However, due to complex package dependencies, we're currently using the general functions from the basic
+	// Cosmos SDK in Gaia. As a temporary solution, we're using Celinium's EncodingConfig to resolve the issue.
+	encfg := celiniumapp.MakeEncodingConfig()
+	return &chain{
+		ID:      "gaia-chain-" + tmrand.Str(6),
+		dataDir: tmpDir,
+		encfg: encodingConfig{
+			InterfaceRegistry: encfg.InterfaceRegistry,
+			Codec:             encfg.Codec,
+			TxConfig:          encfg.TxConfig,
+			Amino:             encfg.Amino,
+		},
+		InitBalance:     "110000000000stake,100000000000000000photon,100000000000000000uatom",
+		Denom:           "uatom",
+		ChainNodeBinary: "gaiad",
+		ModuleBasicsGenesis: func() (json.RawMessage, error) {
+			_, curPath, _, ok := runtime.Caller(0)
+			if !ok {
+				return nil, errors.New("can't get file path from runtime")
+			}
+
+			gaiaGenesisFilePath := filepath.Join(filepath.Dir(curPath), "config", "gaia_default_module_genesis.json")
+			genesiData, err := os.ReadFile(gaiaGenesisFilePath)
+			if err != nil {
+				return nil, err
+			}
+
+			return genesiData, nil
+		},
+		DokcerImage: "cosmos/gaiad-e2e",
+	}, nil
+}
+
+func newCeliniumChain() (*chain, error) {
+	tmpDir, err := os.MkdirTemp("", "celinium-e2e-testnet-")
+	if err != nil {
+		return nil, err
+	}
+
+	encfg := celiniumapp.MakeEncodingConfig()
+	return &chain{
+		ID:      "celin-chain-" + tmrand.Str(6),
+		dataDir: tmpDir,
+		encfg: encodingConfig{
+			InterfaceRegistry: encfg.InterfaceRegistry,
+			Codec:             encfg.Codec,
+			TxConfig:          encfg.TxConfig,
+			Amino:             encfg.Amino,
+		},
+		InitBalance:     "100000000000000000celi",
+		Denom:           "celi",
+		ChainNodeBinary: "celiniumd",
+		ModuleBasicsGenesis: func() (json.RawMessage, error) {
+			return json.MarshalIndent(celiniumapp.ModuleBasics.DefaultGenesis(encfg.Codec), "", " ")
+		},
+		DokcerImage: "celinium", // TODO Use the local docker image compiled by the current code instead
+	}, nil
+}
+
+func (c *chain) configDir() string {
+	return fmt.Sprintf("%s/%s", c.dataDir, c.ID)
+}
+
+func (c *chain) createAndInitValidators(count int) error {
+	for i := 0; i < count; i++ {
+		node := c.createValidator(i)
+
+		// generate genesis files
+		if err := node.init(); err != nil {
+			return err
+		}
+
+		c.validators = append(c.validators, node)
+
+		// create keys
+		if err := node.createKey("val"); err != nil {
+			return err
+		}
+		if err := node.createNodeKey(); err != nil {
+			return err
+		}
+		if err := node.createConsensusKey(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *chain) createAndInitValidatorsWithMnemonics(count int, mnemonics []string) error { //nolint:unused // this is called during e2e tests
+	for i := 0; i < count; i++ {
+		// create node
+		node := c.createValidator(i)
+
+		// generate genesis files
+		if err := node.init(); err != nil {
+			return err
+		}
+
+		c.validators = append(c.validators, node)
+
+		// create keys
+		if err := node.createKeyFromMnemonic("val", mnemonics[i]); err != nil {
+			return err
+		}
+		if err := node.createNodeKey(); err != nil {
+			return err
+		}
+		if err := node.createConsensusKey(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *chain) createValidator(index int) *validator {
+	return &validator{
+		chain:   c,
+		index:   index,
+		moniker: fmt.Sprintf("%s-gaia-%d", c.ID, index),
+	}
+}
--- tests/e2e/e2e_setup_test.go
+++ tests/e2e/e2e_setup_test.go
@@ -1,9 +1,355 @@
-package tests
+package e2e
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
+
+	tmconfig "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/server"
+	srvconfig "github.com/cosmos/cosmos-sdk/server/config"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+)
+
+const (
+	relayerAccountIndex = 0
+	txCommand           = "tx"
+	numberOfEvidences   = 10
+	gas                 = 200000
+	minGasPrice         = "0.00001"
+	gaiaHomePath        = "/home/nonroot/.gaia"
+	keysCommand         = "keys"
+)
+
+var (
+	stakingAmount     = sdk.NewInt(100000000000)
+	standardFeeAmount = sdk.NewInt(330000)
 )
 
 type IntegrationTestSuite struct {
 	suite.Suite
+
+	tmpDirs         []string
+	gaiaChain       *chain
+	celiChain       *chain
+	dkrPool         *dockertest.Pool
+	dkrNet          *dockertest.Network
+	relayerResource *dockertest.Resource
+	valResources    map[string][]*dockertest.Resource
+}
+
+type AddressResponse struct {
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Address  string `json:"address"`
+	Mnemonic string `json:"mnemonic"`
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up e2e integration test suite...")
+
+	var err error
+	s.gaiaChain, err = newGaiaChain()
+	s.Require().NoError(err)
+
+	s.celiChain, err = newCeliniumChain()
+	s.Require().NoError(err)
+
+	s.dkrPool, err = dockertest.NewPool("")
+	s.Require().NoError(err)
+
+	s.dkrNet, err = s.dkrPool.CreateNetwork(fmt.Sprintf("%s-%s-testnet", s.gaiaChain.ID, s.celiChain.ID))
+	s.Require().NoError(err)
+
+	s.valResources = make(map[string][]*dockertest.Resource)
+
+	vestingMnemonic, err := createMnemonic()
+	s.Require().NoError(err)
+
+	jailedValMnemonic, err := createMnemonic()
+	s.Require().NoError(err)
+
+	// The boostrapping phase is as follows:
+	//
+	// 1. Initialize Gaia validator nodes.
+	// 2. Create and initialize Gaia validator genesis files (both chains)
+	// 3. Start both networks.
+	// 4. Create and run IBC relayer containers.
+
+	s.T().Logf("starting e2e infrastructure for chain A; chain-id: %s; datadir: %s", s.gaiaChain.ID, s.gaiaChain.dataDir)
+	s.initNodes(s.gaiaChain)
+	s.initGenesis(s.gaiaChain, vestingMnemonic, jailedValMnemonic)
+	s.initValidatorConfigs(s.gaiaChain)
+	s.runValidators(s.gaiaChain, 0)
+
+	s.T().Logf("starting e2e infrastructure for chain B; chain-id: %s; datadir: %s", s.celiChain.ID, s.celiChain.dataDir)
+	s.initNodes(s.celiChain)
+	s.initGenesis(s.celiChain, vestingMnemonic, jailedValMnemonic)
+	s.initValidatorConfigs(s.celiChain)
+	s.runValidators(s.celiChain, 10)
+
+	time.Sleep(10 * time.Second)
+	s.runIBCRelayer()
+}
+
+func (s *IntegrationTestSuite) initNodes(c *chain) {
+	s.Require().NoError(c.createAndInitValidators(2))
+	/* Adding 4 accounts to val0 local directory
+	c.genesisAccounts[0]: Relayer Wallet
+	c.genesisAccounts[1]: ICA Owner
+	c.genesisAccounts[2]: Test Account 1
+	c.genesisAccounts[3]: Test Account 2
+	*/
+	s.Require().NoError(c.addAccountFromMnemonic(4))
+	// Initialize a genesis file for the first validator
+	val0ConfigDir := c.validators[0].configDir()
+	var addrAll []sdk.AccAddress
+	for _, val := range c.validators {
+		address, _ := val.keyRecord.GetAddress()
+		addrAll = append(addrAll, address)
+	}
+
+	for _, addr := range c.genesisAccounts {
+		acctAddr, _ := addr.keyRecord.GetAddress()
+		addrAll = append(addrAll, acctAddr)
+	}
+
+	s.Require().NoError(
+		modifyGenesis(c.encfg.Codec, val0ConfigDir, "", c.InitBalance, addrAll, c.Denom),
+	)
+	// copy the genesis file to the remaining validators
+	for _, val := range c.validators[1:] {
+		_, err := copyFile(
+			filepath.Join(val0ConfigDir, "config", "genesis.json"),
+			filepath.Join(val.configDir(), "config", "genesis.json"),
+		)
+		s.Require().NoError(err)
+	}
+}
+
+func (s *IntegrationTestSuite) initGenesis(c *chain, vestingMnemonic, jailedValMnemonic string) {
+	var (
+		serverCtx = server.NewDefaultContext()
+		config    = serverCtx.Config
+		validator = c.validators[0]
+	)
+
+	config.SetRoot(validator.configDir())
+	config.Moniker = validator.moniker
+
+	genFilePath := config.GenesisFile()
+	appGenState, genDoc, err := genutiltypes.GenesisStateFromGenFile(genFilePath)
+	s.Require().NoError(err)
+
+	var evidenceGenState evidencetypes.GenesisState
+	s.Require().NoError(c.encfg.Codec.UnmarshalJSON(appGenState[evidencetypes.ModuleName], &evidenceGenState))
+
+	evidenceGenState.Evidence = make([]*codectypes.Any, 10)
+	for i := range evidenceGenState.Evidence {
+		pk := ed25519.GenPrivKey()
+		evidence := &evidencetypes.Equivocation{
+			Height:           1,
+			Power:            100,
+			Time:             time.Now().UTC(),
+			ConsensusAddress: sdk.ConsAddress(pk.PubKey().Address().Bytes()).String(),
+		}
+		evidenceGenState.Evidence[i], err = codectypes.NewAnyWithValue(evidence)
+		s.Require().NoError(err)
+	}
+
+	appGenState[evidencetypes.ModuleName], err = c.encfg.Codec.MarshalJSON(&evidenceGenState)
+	s.Require().NoError(err)
+
+	var genUtilGenState genutiltypes.GenesisState
+	s.Require().NoError(c.encfg.Codec.UnmarshalJSON(appGenState[genutiltypes.ModuleName], &genUtilGenState))
+
+	// generate genesis txs
+	genTxs := make([]json.RawMessage, len(c.validators))
+	for i, val := range c.validators {
+		createValmsg, err := val.buildCreateValidatorMsg(sdk.NewCoin(c.Denom, stakingAmount))
+		s.Require().NoError(err)
+		signedTx, err := val.signMsg(createValmsg)
+
+		s.Require().NoError(err)
+
+		txRaw, err := c.encfg.Codec.MarshalJSON(signedTx)
+		s.Require().NoError(err)
+
+		genTxs[i] = txRaw
+	}
+
+	genUtilGenState.GenTxs = genTxs
+
+	appGenState[genutiltypes.ModuleName], err = c.encfg.Codec.MarshalJSON(&genUtilGenState)
+	s.Require().NoError(err)
+
+	genDoc.AppState, err = json.MarshalIndent(appGenState, "", "  ")
+	s.Require().NoError(err)
+
+	bz, err := tmjson.MarshalIndent(genDoc, "", "  ")
+	s.Require().NoError(err)
+
+	// vestingPeriod, err := generateVestingPeriod()
+	// s.Require().NoError(err)
+
+	rawTx, _, err := buildRawTx(c)
+	s.Require().NoError(err)
+
+	// write the updated genesis file to each validator.
+	for _, val := range c.validators {
+		err = writeFile(filepath.Join(val.configDir(), "config", "genesis.json"), bz)
+		s.Require().NoError(err)
+
+		err = writeFile(filepath.Join(val.configDir(), rawTxFile), rawTx)
+		s.Require().NoError(err)
+	}
+}
+
+// initValidatorConfigs initializes the validator configs for the given chain.
+func (s *IntegrationTestSuite) initValidatorConfigs(c *chain) {
+	for i, val := range c.validators {
+		tmCfgPath := filepath.Join(val.configDir(), "config", "config.toml")
+
+		vpr := viper.New()
+		vpr.SetConfigFile(tmCfgPath)
+		s.Require().NoError(vpr.ReadInConfig())
+
+		valConfig := tmconfig.DefaultConfig()
+
+		s.Require().NoError(vpr.Unmarshal(valConfig))
+
+		valConfig.P2P.ListenAddress = "tcp://0.0.0.0:26656"
+		valConfig.P2P.AddrBookStrict = false
+		valConfig.P2P.ExternalAddress = fmt.Sprintf("%s:%d", val.instanceName(), 26656)
+		valConfig.RPC.ListenAddress = "tcp://0.0.0.0:26657"
+		valConfig.StateSync.Enable = false
+		valConfig.LogLevel = "info"
+
+		var peers []string
+
+		for j := 0; j < len(c.validators); j++ {
+			if i == j {
+				continue
+			}
+
+			peer := c.validators[j]
+			peerID := fmt.Sprintf("%s@%s%d:26656", peer.nodeKey.ID(), peer.moniker, j)
+			peers = append(peers, peerID)
+		}
+
+		valConfig.P2P.PersistentPeers = strings.Join(peers, ",")
+
+		tmconfig.WriteConfigFile(tmCfgPath, valConfig)
+
+		appConfig := srvconfig.DefaultConfig()
+		appConfig.API.Enable = true
+		appConfig.MinGasPrices = fmt.Sprintf("%s%s", minGasPrice, c.Denom)
+
+		customAppTemplate := `
+###############################################################################
+###                        Custom Gaia Configuration                        ###
+###############################################################################
+# bypass-min-fee-msg-types defines custom message types the operator may set that
+# will bypass minimum fee checks during CheckTx.
+#
+# Example:
+# ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", ...]
+bypass-min-fee-msg-types = ["/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward","/ibc.applications.transfer.v1.MsgTransfer"]
+` + srvconfig.DefaultConfigTemplate
+		srvconfig.SetConfigTemplate(customAppTemplate)
+	}
+}
+
+// runValidators runs the validators in the chain
+func (s *IntegrationTestSuite) runValidators(c *chain, portOffset int) {
+	s.T().Logf("starting chain %s validator containers...", c.ID)
+
+	s.valResources[c.ID] = make([]*dockertest.Resource, len(c.validators))
+	for i, val := range c.validators {
+		runOpts := &dockertest.RunOptions{
+			Name:      val.instanceName(),
+			NetworkID: s.dkrNet.Network.ID,
+			Mounts: []string{
+				fmt.Sprintf("%s/:%s", val.configDir(), gaiaHomePath),
+			},
+			Repository: "cosmos/gaiad-e2e",
+		}
+
+		s.Require().NoError(exec.Command("chmod", "-R", "0777", val.configDir()).Run()) //nolint:gosec // this is a test
+
+		// expose the first validator for debugging and communication
+		if val.index == 0 {
+			runOpts.PortBindings = map[docker.Port][]docker.PortBinding{
+				"1317/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 1317+portOffset)}},
+				"6060/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6060+portOffset)}},
+				"6061/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6061+portOffset)}},
+				"6062/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6062+portOffset)}},
+				"6063/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6063+portOffset)}},
+				"6064/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6064+portOffset)}},
+				"6065/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 6065+portOffset)}},
+				"9090/tcp":  {{HostIP: "", HostPort: fmt.Sprintf("%d", 9090+portOffset)}},
+				"26656/tcp": {{HostIP: "", HostPort: fmt.Sprintf("%d", 26656+portOffset)}},
+				"26657/tcp": {{HostIP: "", HostPort: fmt.Sprintf("%d", 26657+portOffset)}},
+			}
+		}
+
+		resource, err := s.dkrPool.RunWithOptions(runOpts, noRestart)
+		s.Require().NoError(err)
+
+		s.valResources[c.ID][i] = resource
+		s.T().Logf("started Gaia %s validator container: %s", c.ID, resource.Container.ID)
+	}
+
+	rpcClient, err := rpchttp.New("tcp://localhost:26657", "/websocket")
+	s.Require().NoError(err)
+
+	s.Require().Eventually(
+		func() bool {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+
+			status, err := rpcClient.Status(ctx)
+			if err != nil {
+				return false
+			}
+
+			// let the node produce a few blocks
+			if status.SyncInfo.CatchingUp || status.SyncInfo.LatestBlockHeight < 3 {
+				return false
+			}
+
+			return true
+		},
+		5*time.Minute,
+		time.Second,
+		"chain node failed to produce blocks",
+	)
+}
+
+func noRestart(config *docker.HostConfig) {
+	// in this case we don't want the nodes to restart on failure
+	config.RestartPolicy = docker.RestartPolicy{
+		Name: "no",
+	}
 }
